778fe99f25bf1e5b119fa1da7546755f000f22ce
julio cesa caro <juliojr@gmail.com>
2023-10-25 19:44:02 -0300

    bart

---
+++ julio.js
@@ -0,0 +1,14 @@
+let prevBtn = document.querySelector(".prev-btn");
+
+
+prevBtn.addEventListener("click", () => {
+    let homeroEl = document.querySelector(".homero");
+  homeroEl.style.color = "blue";})
+
+
+
+
+
+
+
+
